a0686237a8cffe9bcfd374b08a0ab9615e3e3f91
Kyrylo <kirilzhm@gmail.com>
2025-03-11 20:31:21 +0100

    added Header-HomePage

---
+++ src/components/Header-HomePage/Header-HomePage.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+    return (
+        <header className="flex items-center justify-between h-[50px] ml-25 mr-25">
+            <Link to="settings">
+                <svg className="size-[20px]">
+                    <use href="/icons.svg#icon-settings"></use>
+                </svg>
+            </Link>
+            <h3 className="font-[700] text-[16px]">Chats</h3>
+            <button className="bg-transparent border-none size-[20px]">
+                <svg className="size-[20px]">
+                    <use href="/icons.svg#icon-search"></use>
+                </svg>
+            </button>
+        </header>
+    )
+}
+
+export default Header;
