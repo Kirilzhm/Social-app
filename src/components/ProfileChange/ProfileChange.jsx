4c5f7005f14f23d58d4b68646a8eb39668b331b1
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2025-03-22 23:31:41 +0200

    add some styles fot form

--- src/components/ProfileChange/ProfileChange.jsx
+++ src/components/ProfileChange/ProfileChange.jsx
@@ -3,6 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useId } from 'react';
 import * as Yup from 'yup';
 import css from './ProfileChange.module.css';
+import { profileStyleBtn, profileStyleSvg } from '../../js/settingStyle';
 
 const FeedbackSchema = Yup.object().shape({
   name: Yup.string()
@@ -38,13 +39,12 @@ export const ProfileChange = () => {
         validationSchema={FeedbackSchema}
         onSubmit={handleSubmit}
       >
-        <Form
-        className='flex flex-col'>
+        <Form className="flex flex-col">
           <label htmlFor={nameFieldId} className="sr-only ">
             Name or Nickname
           </label>
           <Field
-            className="w-[100%] h-[42px] text-[#090202]"
+            className="w-[100%] h-[42px] text-[#090202] text-left"
             type="text"
             name="name"
             id={nameFieldId}
@@ -54,51 +54,74 @@ export const ProfileChange = () => {
             SurName
           </label>
           <Field
-            className="w-[100%] h-[42px]"
+            className="w-[100%] h-[42px] text-left"
             type="text"
             name="surName"
             id={surNameFieldId}
             placeholder="Surname"
           ></Field>
-          <label
-            htmlFor={telephoneFieldId}
-            className="text-[10px] font-normal text-[#090202]"
-          >
-            Telephone
-          </label>
-          <Field
-            className="max-w-[150px] h-[15px]"
-            type="text"
-            name="telephone"
-            id={telephoneFieldId}
-            placeholder="Telephone"
-          ></Field>
-          <label
-            htmlFor={emailFieldId}
-            className="text-[10px] font-normal text-[#090202]"
-          >
-            Email
-          </label>
-          <Field
-            className="max-w-[150px] h-[15px]"
-            type="text"
-            name="email"
-            id={emailFieldId}
-            placeholder="Email"
-          ></Field>
-          <label
-            htmlFor={dateFieldId}
-            className="text-[10px] font-normal text-[#090202]"
-          >
-            Date of birth
-          </label>
-          <Field
-            className="max-w-[150px] h-[15px]"
-            type="text"
-            name="date"
-            id={dateFieldId}
-            placeholder="Date of birth"
-          ></Field>
+          <div className={css.inputWrapper}>
+            <label
+              htmlFor={telephoneFieldId}
+              className="text-[10px] font-normal text-[#090202]"
+            >
+              Telephone
+            </label>
+            <Field
+              className="w-[180px] h-[15px]"
+              type="text"
+              name="telephone"
+              id={telephoneFieldId}
+              placeholder="Telephone"
+            ></Field>
+          </div>
+
+          <div className={css.inputWrapper}>
+            <label
+              htmlFor={emailFieldId}
+              className="text-[10px] font-normal text-[#090202]"
+            >
+              Email
+            </label>
+            <Field
+              className="w-[180px] h-[15px]"
+              type="text"
+              name="email"
+              id={emailFieldId}
+              placeholder="Email"
+            ></Field>
+          </div>
+          <div className={css.inputWrapper}>
+            <label
+              htmlFor={dateFieldId}
+              className="text-[10px] font-normal text-[#090202]"
+            >
+              Date of birth
+            </label>
+            <Field
+              className="w-[180px] h-[15px]"
+              type="text"
+              name="date"
+              id={dateFieldId}
+              placeholder="Date of birth"
+            ></Field>
+          </div>
+          <div className="flex flex-row">
+            <button type="submit" className={profileStyleBtn()}>
+              <svg className={profileStyleSvg()}>
+                <use href="./icons.svg#icon-Frame-10"></use>
+              </svg>
+            </button>
+            <button
+              type="button"
+              className={profileStyleBtn()}
+              onClick={() => alert('Declined')}
+            >
+              <svg className={profileStyleSvg()}>
+                <use href="./icons.svg#icon-Frame-7-3"></use>
+              </svg>
+            </button>
+          </div>
         </Form>
       </Formik>
     </div>
